4383bfb2557d7b107b188721f83bb6f236e08263
Luis Martinez <luis_mmartinez@live.com>
2021-01-03 01:09:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,14 +13,14 @@ class EmailAddressParser
   end
 
   def parse
-    if @email_addresses.include?(",") && @email_addresses.include?(" ")
+  #  if @email_addresses.include?(",") && @email_addresses.include?(" ")
       parsed_array = @email_addresses.split(/[(,)( )]/).delete_if {|i| i == ""}.uniq
-    elsif @email_addresses.include?(",")
-      parsed_array = @email_addresses.split(",").collect {|i| i.strip}.uniq
-    elsif @email_addresses.include?(" ")
-      parsed_array = @email_addresses.split(" ").collect {|i| i.strip}.uniq
-    end
-
+  #  elsif @email_addresses.include?(",")
+  #    parsed_array = @email_addresses.split(",").collect {|i| i.strip}.uniq
+  #  elsif @email_addresses.include?(" ")
+  #    parsed_array = @email_addresses.split(" ").collect {|i| i.strip}.uniq
+  #  end
+binding.pry
   end
 
 end
